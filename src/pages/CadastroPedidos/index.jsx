eb7cdea88b9b87ae1506552f9f595063c1e5e89a
Iago Luan <iago.luancj@gmail.com>
2024-06-11 23:11:29 -0300

    dale

--- src/pages/CadastroPedidos/index.jsx
+++ src/pages/CadastroPedidos/index.jsx
@@ -3,10 +3,17 @@ import Link from "next/link";
 export default function CadastroPedidos() {
     return (
         <>
-            <div>
-                Cadastro
-            </div>
-                <Link href="/Cadastro"></Link>
+            <form>    
+                    <p>Numero do Pedido: gera autmatico</p>       
+                    <p>Status: checkbox</p>
+                    <p>Nome do Cliente: <input type="text" /> </p>
+                    <p>Nome do Garçom: checkbox</p>
+                    <p>Nome do Prato: checkbox</p>
+                    <p>Observação: <textarea name="" id=""></textarea></p>
+            </form>
+            <button>submit</button>
+            <Link href="/">Voltar</Link>
+
         </>
     )
 }
--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,7 @@
 import { create } from 'zustand'
 
+// matriz de pedidos, de funcionarios e do cardapio. 
+
 export const useRequests = create((set) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
