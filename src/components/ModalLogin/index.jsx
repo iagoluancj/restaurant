4708d07871dc2ce765d915854d6401d8fcf31098
iagoluancj <iago.luancj@gmail.com>
2024-07-02 15:38:57 -0300

    radix

--- src/components/ModalLogin/index.jsx
+++ src/components/ModalLogin/index.jsx
@@ -1,46 +1,45 @@
-import { useState } from "react";
-import { Close, Modal, ModalContent } from "./styles"
+import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import './styles.css';
 
-export default function ModalLogin() {
-    let modal, btn, span = false
+const ModalLogin = () => (
+  <Dialog.Root>
+    <Dialog.Trigger asChild>
+      <button className="Button violet">Edit profile</button>
+    </Dialog.Trigger>
+    <Dialog.Portal>
+      <Dialog.Overlay className="DialogOverlay" />
+      <Dialog.Content className="DialogContent">
+        <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
+        <Dialog.Description className="DialogDescription">
+          Make changes to your profile here. Click save when you're done.
+        </Dialog.Description>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="name">
+            Name
+          </label>
+          <input className="Input" id="name" defaultValue="Pedro Duarte" />
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="username">
+            Username
+          </label>
+          <input className="Input" id="username" defaultValue="@peduarte" />
+        </fieldset>
+        <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
+          <Dialog.Close asChild>
+            <button className="Button green">Save changes</button>
+          </Dialog.Close>
+        </div>
+        <Dialog.Close asChild>
+          <button className="IconButton" aria-label="Close">
+            <Cross2Icon />
+          </button>
+        </Dialog.Close>
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
 
-    const [modalProps, setModalProps] = useState([modal, btn, span])
-    // var modal = document.getElementById("myModal");
-
-    // // Get the button that opens the modal
-    // var btn = document.getElementById("myBtn");
-
-    // // Get the <span> element that closes the modal
-    // var span = document.getElementsByClassName("close")[0];
-
-    // // When the user clicks on the button, open the modal
-    // btn.onclick = function() {
-    // modal.style.display = "block";
-    // }
-
-    // // When the user clicks on <span> (x), close the modal
-    // span.onclick = function() {
-    // modal.style.display = "none";
-    // }
-
-    // // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function(event) {
-    // if (event.target == modal) {
-    //     modal.style.display = "none";
-    // }
-    // }
-
-    return (
-        <>
-            <button id="myBtn">Open Modal</button>
-            <div>
-                <Modal id="myModal" class="modal">
-                <ModalContent class="modal-content">
-                    <Close class="close">&times;</Close>
-                    <p>Some text in the Modal..</p>
-                </ModalContent>
-                </Modal>
-            </div>
-        </>
-    )
-}
+export default ModalLogin;
